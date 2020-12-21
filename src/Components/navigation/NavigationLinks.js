881610e67d5994bb3d69655ccf41434daf6bef0c
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2020-12-22 00:30:07 +0200

    added homePage  requarest

--- src/Components/navigation/NavigationLinks.js
+++ src/Components/navigation/NavigationLinks.js
@@ -21,27 +21,3 @@ const NavigationLinks = () => {
 };
 
 export default NavigationLinks;
-
-// import React, { Suspense } from "react";
-// import { Switch, Route } from "react-router-dom";
-// import { mainRoutes } from "../../routes/mainRoutes";
-// import Loader from "../loader/Loader";
-
-// const NavigationRoutes = () => {
-//   return (
-//     <Suspense fallback={<Loader />}>
-//       <Switch>
-//         {mainRoutes.map((route) => (
-//           <Route
-//             key={route.path}
-//             path={route.path}
-//             exact={route.exact}
-//             component={route.component}
-//           />
-//         ))}
-//       </Switch>
-//     </Suspense>
-//   );
-// };
-
-// export default NavigationRoutes;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    // Обновляем заголовок документа, используя API браузера
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/movie/day?api_key=6d4314c3991bf14e2dde598365e18999"
+      )
+      .then((response) => {
+        // handle success
+        setMovies([...response.data.results]);
+        // console.log(movies);
+      })
+      .catch(function (error) {
+        // handle error
+        // console.log(error);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {movies.map((item) => {
+        return (
+          <li key={item.id}>
+            <NavLink to={`/${item.id}`} exact={true}>
+              {item.original_title}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Home;
